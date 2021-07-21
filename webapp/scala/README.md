0980c3dc35eb4f076feacf415250b008bb21839f
Windymelt <windymelt@3qe.us>
2021-07-21 23:35:54 +0900

    sbt new scalatra/scalatra.g8

---
+++ webapp/scala/README.md
@@ -0,0 +1,12 @@
+# scala #
+
+## Build & Run ##
+
+```sh
+$ cd scala
+$ sbt
+> jetty:start
+> browse
+```
+
+If `browse` doesn't launch your browser, manually open [http://localhost:8080/](http://localhost:8080/) in your browser.
---
+++ webapp/scala/src/main/scala/ScalatraBootstrap.scala
@@ -0,0 +1,9 @@
+import com.isu.scala._
+import org.scalatra._
+import javax.servlet.ServletContext
+
+class ScalatraBootstrap extends LifeCycle {
+  override def init(context: ServletContext) {
+    context.mount(new MyScalatraServlet, "/*")
+  }
+}
